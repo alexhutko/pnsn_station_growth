5ae028ca48ae4f239ac935045d82afaff33eaa8d
alexhutko <ahutko@gmail.com>
2021-02-03 23:49:58 -0800

    fixed bug

--- stations_through_time.py
+++ stations_through_time.py
@@ -27,7 +27,7 @@ for line in f.iter_lines():
 nowtime = datetime.datetime.now()
 dict_SMA = {}
 dict_BB = {}
-url = 'http://service.iris.edu/fdsnws/station/1/query?level=channel&network=_PACNW&starttime=2100-01-01T00:00:00&format=text'
+url = 'http://service.iris.edu/fdsnws/station/1/query?level=channel&network=_PACNW&endtime=2100-01-01T00:00:00&format=text'
 f = requests.get(url)
 n = 0
 for line in f.iter_lines():
@@ -38,7 +38,7 @@ for line in f.iter_lines():
         net = line.split('|')[0]
         sta = line.split('|')[1]
         cha = line.split('|')[3]
-        if ( sta[0:1] != 'Q' and cha[0:2] in [ 'EN', 'HN', 'BH', 'HH' ] ):
+        if ( sta[0:1] != 'Q' and cha[0:3] in [ 'ENZ', 'HNZ', 'BHZ', 'HHZ' ] ):
             lat = line.split('|')[4]
             lon = line.split('|')[5]
             try:
@@ -47,20 +47,29 @@ for line in f.iter_lines():
             except:
                 endT = datetime.datetime(2100,1,1,0,0,0)
             print(net, sta, cha, lat, lon, startT, endT )
-            if ( cha[0:2] in [ 'EN', 'HN' ] ):
+            if ( cha[0:3] in [ 'ENZ', 'HNZ' ] ):
                 if ( sta in dict_SMA ):
                     if ( startT < dict_SMA[sta][0] ):
-                        dict_SMA[sta] = [ startT, net ]
+                        dict_SMA[sta][0] = startT
+                        if ( sta == 'NOWS' ):
+                            print(dict_SMA[sta] )
+                    if ( endT > dict_SMA[sta][1] ):
+                        dict_SMA[sta][1] = endT
+                        if ( sta == 'NOWS' ):
+                            print(dict_SMA[sta] )
                 else:
-                    dict_SMA[sta] = [ startT, net ]
-            if ( cha[0:2] in [ 'BH', 'HH' ] ):
+                    dict_SMA[sta] = [ startT, endT, net ]
+            if ( cha[0:3] in [ 'BHZ', 'HHZ' ] ):
                 if ( sta in dict_BB ):
                     if ( startT < dict_BB[sta][0] ):
-                        dict_BB[sta] = [ startT, net ]
+                        dict_BB[sta][0] = startT
+                    if ( endT > dict_BB[sta][1] ):
+                        dict_BB[sta][1] = endT
                 else:
-                    dict_BB[sta] = [ startT, net ]
+                    dict_BB[sta] = [ startT, endT, net ]
+            if ( sta == 'NOWS' ):
+                print('XXXX ', startT, endT, cha, dict_SMA[sta] )
 
-print(len(dict_BB),len(dict_SMA))
 
 ndays =  (nowtime - datetime.datetime(2013,1,1,0,0,0)).days
 print(ndays)
@@ -80,24 +89,24 @@ for n in range(0,ndays):
     BBcount = {'UW':0,'UO':0,'CC':0}#,'IU':0,'US':0,'OO':0,'BK':0}
     sixchancount = {'UW':0,'UO':0,'CC':0}#,'IU':0,'US':0,'OO':0,'BK':0} 
     for sta in dict_SMA:
-        net = dict_SMA[sta][1]
-        if ( dict_SMA[sta][0] < thisday and net in nets ):
+        net = dict_SMA[sta][2]
+        if ( dict_SMA[sta][0] < thisday and dict_SMA[sta][1] > nowtime and net in nets ):
             SMAcount[net] +=1
     for sta in dict_BB:
-        net = dict_BB[sta][1]
+        net = dict_BB[sta][2]
         if ( sta in dict_SMA ):
-            if ( dict_SMA[sta][0] < thisday and net in nets ):
+            if ( dict_SMA[sta][0] < thisday and dict_SMA[sta][1] > nowtime and net in nets ):
                 sixchancount[net] += 1
-                SMAcount[net] -=1
+                SMAcount[net] += -1
         else:
-            if ( dict_BB[sta][0] < thisday and net in nets ):
+            if ( dict_BB[sta][0] < thisday and dict_BB[sta][1] > nowtime and net in nets ):
                 BBcount[net] +=1
 
     ySMA.append(sum(SMAcount.values()))
     yBB.append(sum(BBcount.values()))
     ysixchan.append(sum(sixchancount.values()))
     ytotal.append(sum(BBcount.values()) + sum(SMAcount.values()) + sum(sixchancount.values()) )
-##    print(thisday,SMAcount['UW'], sum(BBcount.values()) )
+    print(thisday,SMAcount['UO'], BBcount['UO'], sixchancount['UO'], sum(BBcount.values()) )
 
 ystack = [ ySMA, yBB, ysixchan ]
 figname = 'stations_through_time.png'
